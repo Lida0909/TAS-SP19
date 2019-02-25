6dbf8523c039998cd8624c8c080de8f2159c53c1
Sabin Banjara <sbanjara@stu.jsu.edu>
2019-02-26 00:03:38 +0545

    Change of data types in Shift Class

--- src/cs310/tas/wrf/Shift.java
+++ src/cs310/tas/wrf/Shift.java
@@ -2,96 +2,98 @@
 package cs310.tas.wrf;
 
 import java.sql.Time;
+import java.time.LocalTime;
 
 public class Shift {
     
     private String description; 
-    private Time startingTime;
-    private Time stoppingTime;
-    private Time lunchStart;
-    private Time lunchStop;
+    private LocalTime startingTime;
+    private LocalTime stoppingTime;
+    private LocalTime lunchStart;
+    private LocalTime lunchStop;
     
-    private int lunchDeduct;
-    private int interval;
-    private int gracePeriod;
-    private int dock;
+    private LocalTime lunchDeduct;
+    private LocalTime interval;
+    private LocalTime gracePeriod;
+    private LocalTime dock;
     
-    public Shift(String description, Time startTime,int interval, int gracePeriod,
-            int dock, Time stopTime, Time lunchStart, Time lunchStop, int lunchDeduct) {
+    public Shift(String description, int startHour, int startMin,int interval, int gracePeriod,
+            int dock, int stopHour, int stopMin, int lunchStartHour, int lunchStartMin,
+            int lunchStopHour, int lunchStopMin, int lunchDeduct) {
         
         this.description = description;
-        this.startingTime = startTime;
-        this.stoppingTime = stopTime;
-        this.lunchStart = lunchStart;
-        this.lunchStop = lunchStop;
-        this.lunchDeduct = lunchDeduct ;
-        this.interval = interval;
-        this.gracePeriod = gracePeriod;
-        this.dock = dock;
+        this.startingTime = LocalTime.of(startHour, startMin, 0);
+        this.stoppingTime = LocalTime.of(stopHour, stopMin, 0);
+        this.lunchStart = LocalTime.of(lunchStartHour, lunchStartMin, 0);
+        this.lunchStop = LocalTime.of(lunchStopHour, lunchStopMin, 0);
+        this.lunchDeduct = LocalTime.of(0,lunchDeduct, 0);
+        this.interval = LocalTime.of(0, interval, 0);
+        this.gracePeriod = LocalTime.of(0, gracePeriod, 0);
+        this.dock = LocalTime.of(0, dock, 0);
         
     }
     
-    public Time getStartingTime() {
+    public LocalTime getStartingTime() {
         return startingTime;
     }
 
-    public void setStartingTime(Time startingTime) {
+    public void setStartingTime(LocalTime startingTime) {
         this.startingTime = startingTime;
     }
 
-    public Time getStoppingTime() {
+    public LocalTime getStoppingTime() {
         return stoppingTime;
     }
 
-    public void setStoppingTime(Time stoppigTime) {
+    public void setStoppingTime(LocalTime stoppigTime) {
         this.stoppingTime = stoppigTime;
     }
 
-    public Time getLunchStart() {
+    public LocalTime getLunchStart() {
         return lunchStart;
     }
 
-    public void setLunchStart(Time lunchStart) {
+    public void setLunchStart(LocalTime lunchStart) {
         this.lunchStart = lunchStart;
     }
 
-    public Time getLunchStop() {
+    public LocalTime getLunchStop() {
         return lunchStop;
     }
 
-    public void setLunchStop(Time lunchStop) {
+    public void setLunchStop(LocalTime lunchStop) {
         this.lunchStop = lunchStop;
     }
 
-    public int lunchDeduct() {
+    public LocalTime lunchDeduct() {
         return lunchDeduct;
     }
 
-    public void setLunchDeduct(int lunchDeduct) {
+    public void setLunchDeduct(LocalTime lunchDeduct) {
         this.lunchDeduct = lunchDeduct;
     }
 
-    public int getInterval() {
+    public LocalTime getInterval() {
         return interval;
     }
 
-    public void setInterval(int interval) {
+    public void setInterval(LocalTime interval) {
         this.interval = interval;
     }
 
-    public int getGracePeriod() {
+    public LocalTime getGracePeriod() {
         return gracePeriod;
     }
 
-    public void setGracePeriod(int gracePeriod) {
+    public void setGracePeriod(LocalTime gracePeriod) {
         this.gracePeriod = gracePeriod;
     }
 
-    public int getDock() {
+    public LocalTime getDock() {
         return dock;
     }
 
-    public void setDock(int dock) {
+    public void setDock(LocalTime dock) {
         this.dock = dock;
     }
     
