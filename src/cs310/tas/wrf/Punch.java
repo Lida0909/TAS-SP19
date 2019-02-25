da810eac48c2687384ef99d28e0c0d514b294ac8
Marcus Shell <mshell2stu.jsu.edu>
2019-02-24 19:22:38 -0600

    fixed package names and compiled name to clear error messages.
    I added notes in the file to hint what I think should be done.
    Take a look at the notes and see if you agree with my suggestions or
    if they make sense to you.

--- src/cs310/tas/wrf/Punch.java
+++ src/cs310/tas/wrf/Punch.java
@@ -14,11 +14,12 @@ import java.util.*;
  */
 public class Punch {
     
-    private Badge badge = new Badge(String, String);
+    private Badge badge;// = new Badge(String, String);
     private int terminalID;
     private int punchTypeID;
     private int id;
     private String description;
+    //make a private Gregoriancalendar object "timestamp" or "cal"  here
         
     Punch (Badge badge, int terminalID, int punchTypeID){
         
@@ -26,13 +27,22 @@ public class Punch {
         this.terminalID = terminalID;
         this.punchTypeID = punchTypeID;
         
+        /*
+        instantiate our private GregorianCalendar object here by setting it to the current time
+        according to https://www.geeksforgeeks.org/java-util-gregoriancalendar-class-java/ :
+        it looks as if using the constructor with empty paremeters is correct.
+        However, if you declare the Gregorian calendar object above, then you don't need to re-delcare the class type here so you can just use
+        "cal = new GregorianCalendar()" or "timestamp = new GregorianCalendar()"
+        */
+        
         GregorianCalendar Cal = new GregorianCalendar();
      
         
     }
     
     public String printOriginalTimeSstamp() {
-        
+        //if we set a class variable of type GregorianCalendar , we can just use a command to "getTheTimeAndDate" from "timestamp" and return that or print it.
+        //I think we need to also have a class that returns the "timestamp" itself as a gregorian calendar object. like  "public GregorianCalendare methodname(){pass the caller the gregorianCalendar object}
         GregorianCalendar Cal = new GregorianCalendar();
         
          
