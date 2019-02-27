10dced2e19f6c235619682c129f5627b8a9de9de
Adam Stith <Adam Stith@DESKTOP-PFLR7VN>
2019-02-27 06:55:26 -0600

    Reverting Punch class to what it was after initial changes(@Marcus i was never able to see your changes)

--- src/cs310/tas/wrf/Punch.java
+++ src/cs310/tas/wrf/Punch.java
@@ -1,63 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs310.tas.wrf;
 
-import cs310.tas.wrf.Badge;
+import java.sql.Timestamp;
 import java.util.*;
 
 /**
  *
- * @author Matt Denman and William Penwell
+ * @author Matt Denman and William Penwell, Adam Stith
  */
 public class Punch {
     
-    private Badge badge;// = new Badge(String, String);
+    private String badgeID;
     private int terminalID;
     private int punchTypeID;
     private int id;
-    private String description;
-    //make a private Gregoriancalendar object "timestamp" or "cal"  here
+    private Timestamp originalTimeStamp;
         
-    Punch (Badge badge, int terminalID, int punchTypeID){
+    Punch (int id, int terminalID, String badgeID, Timestamp originalTimeStamp, int punchTypeID) {
         
-        this.badge = badge;
+        this.id = id;
         this.terminalID = terminalID;
+        this.badgeID = badgeID;
+        this.originalTimeStamp = originalTimeStamp;
         this.punchTypeID = punchTypeID;
-        
-        /*
-        instantiate our private GregorianCalendar object here by setting it to the current time
-        according to https://www.geeksforgeeks.org/java-util-gregoriancalendar-class-java/ :
-        it looks as if using the constructor with empty paremeters is correct.
-        However, if you declare the Gregorian calendar object above, then you don't need to re-delcare the class type here so you can just use
-        "cal = new GregorianCalendar()" or "timestamp = new GregorianCalendar()"
-        */
-        
-        GregorianCalendar Cal = new GregorianCalendar();
      
-        
     }
     
-    public String printOriginalTimeSstamp() {
+    public String printOriginalTimestamp() {
         //if we set a class variable of type GregorianCalendar , we can just use a command to "getTheTimeAndDate" from "timestamp" and return that or print it.
         //I think we need to also have a class that returns the "timestamp" itself as a gregorian calendar object. like  "public GregorianCalendare methodname(){pass the caller the gregorianCalendar object}
-        GregorianCalendar Cal = new GregorianCalendar();
+        String punchResults = "";
+        GregorianCalendar cal = new GregorianCalendar();
+        cal.setTimeInMillis(originalTimeStamp.getTime());
+        Date date = cal.getTime();
         
+        switch(this.punchTypeID){
+            case 0:
+                punchResults = "CLOCKED OUT: ";
+                break;
+            case 1:
+                punchResults = "CLOCKED IN: ";
+                break;
+            case 2:
+                punchResults = "TIMED OUT: ";
+                break;
+            default:
+                System.out.println("ERROR");
+        }
+        
+        String originalTimestamptoString = "#" + getBadgeID() + punchResults + " " + date;
+        
+        return originalTimestamptoString;
          
-        return "";
     }
     
     public String printAdjustedTimeStamp() {
         return "";
     }
-    
-    Punch (int id, String description) {
-        
-        this.id = id;
-        this.description = description;
+
+    public String getBadgeID() {
+        return badgeID;
     }
+    
+    
     
 }
 
