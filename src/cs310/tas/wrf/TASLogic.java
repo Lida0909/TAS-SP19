6f8c055a1197af36a334f65bbb2667a6bf0c93ad
Marcus Shell <44873263+Mshell0126@users.noreply.github.com>
2019-03-22 11:26:56 -0500

    Feature 4 (#6)
    * Feature 3 (#3) (#4)
    
    * Feature 3 (#3)
    
    * Cleaned up the overloaded constructor in the Punch class
    
    * Added feature 3 Test
    
    * Fixed the outdated getBadgeid() and the items that depend on it. Also fixed the Punch creation in TAS Database getPunch() method
    
    * Merged MasterBranch with Feature 2 elements added onto feature 3 so it is up to date.
    
    * working on adjust method
    
    * commit to revert changes made to wrong branch
    
    * Implementation of the adjust() in the punch class
    
    * Some additional updates in the Adjust() method of punch class and TASDatabase class
    
    * java docs for shift class
    
    * Implementation of Weekend logic in adjust()
    
    * Removed gitignore
    
    * Made the following adjustments:
    
    -created the JavaDocs for the TASDatabase class
    -corrected misspellings in setStoppingTime() method's argument
    
    * Made the following adjustments:
    
    -removed extra lines and unused exports from Shift class
    -updated JavaDocs in Badge class
    -removed extra lines from TASLogic class
    -started work on JavaDocs in Punch class
    -changed all @authors to "War Room F"
    
    * Changes in TASDatabase and Punch class
    
    * fixed java doc for shift (please check if you can to make sure done 
    properly)
    
    * fixed java doc for punch (please check to see if everything is like we
    want it)
    
    * double checked the work to see if anything obvious broke when doing the last merge. Everything seems ok.
    
    replaced a missing return statement
    
    * Added Feature4 Test file
    
    * Add files via upload
    
    * feature 4
    
    * The following changes were made:
    
    -removed work around for difference in system clock in Punch class
    -added check for a clock in-clock out pair going over to a different day
    -added calculateTotalMinutes() method to TASLogic class
    
    * -removed divide by 1000 from getOriginalTimestamp() method
    -added lunchTime static varibale
    
    * format fix for system clock millis
    
    * Merge origin/master into Feature-4
    
    Conflicts:
    	src/cs310/tas/wrf/TASLogic.java
    
    * added JavaDocs to TASLogic
    
    * fix javaDocs for TASLogic
    
    * Deleted accidental local copy

--- src/cs310/tas/wrf/TASLogic.java
+++ src/cs310/tas/wrf/TASLogic.java
@@ -1,9 +1,83 @@
 package cs310.tas.wrf;
 
+import java.util.ArrayList;
+
 /**
- *
+ * The TASLogic class contains functions that will be needed for use by  
+ * payroll, mangers, etc.
  * @author War Room F
  */
 public class TASLogic {
+    
+    /**
+     * Constant variable for the punch type of a clock-in.
+     */
+    public static final int CLOCKIN = 1;
+
+    /**
+     * Constant variable for the punch type of a clock-out.
+     */
+    public static final int CLOCKOUT = 0;
+    
+    /**
+     * Calculates the total minutes accrued by an employee on a single shift.
+     * @param dailypunchlist an ArrayList of Punch objects for a shift
+     * @param shift a shift object containing the shift rules
+     * @return the total amount of minutes accrued in one shift as an int
+     */
+    public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, 
+            Shift shift) {
+
+        int totalMin = 0;
+        long totalMillis = 0;
+        long inTime = 0;
+        long outTime = 0;
+        int punchCounter = 0;
+        int lunchTime = 30;
+        
+        for(int i = 0; i < dailypunchlist.size(); i++) {
+            
+           if (dailypunchlist.get(i).getPunchtypeid() == CLOCKIN) {
+               
+               inTime = dailypunchlist.get(i).getAdjustedTimeStamp().getTime();
+               punchCounter++;
+               continue;
+               
+           }
+
+           if (dailypunchlist.get(i).getPunchtypeid() == CLOCKOUT) {
+               
+               outTime = dailypunchlist.get(i).getAdjustedTimeStamp().getTime();
+               punchCounter++;
+               
+           }
+           
+           if (inTime != 0 && outTime != 0) {
+               
+               totalMillis += outTime - inTime;
+               
+           }
+           
+           
+           inTime = 0;
+           outTime = 0;
+           
+        }
+        
+        if (totalMillis != 0) {
+            
+            totalMin = (int) (totalMillis/60000);
+            
+        }
+        
+        if (totalMin > shift.getlunchDeduct() && punchCounter <= 3) {
+            
+            totalMin -= lunchTime;
+            
+        }
+        
+        return totalMin;
+        
+    }    
     
 }
