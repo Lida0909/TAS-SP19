fb56164a4e99ebbeb2561c350c7958f09cd07b74
Michael Green <hoosie54@gmail.com>
2019-04-15 12:06:23 -0500

    made javadocs for Shift descriptions for Feature-7

--- src/cs310/tas/wrf/Shift.java
+++ src/cs310/tas/wrf/Shift.java
@@ -23,7 +23,9 @@ public class Shift {
     private int dock;
     
     /**
-     * 
+     * Sets the description, start hour, start minute, interval, grace period,
+     * dock, stop hour, stop minute, lunch start hour, lunch start minute, lunch
+     * stop hour, lunch stop minute, and lunch deduct
      * @param description a String that represents the shift type
      * @param startHour an int that represents the start hour of the shift
      * @param startMin an int that represents the start minute of the shift
@@ -60,7 +62,7 @@ public class Shift {
     /* Getter Methods */
     
     /**
-     * 
+     * Fetches the starting time of a specific shift
      * @return the Starting time or the shift as a LocalTime object
      */
     public LocalTime getStartingTime() {
@@ -68,7 +70,7 @@ public class Shift {
     }
     
     /**
-     *
+     * Fetches the starting time hour of a specific shift
      * @return the starting hour of the shift as an int
      */
     public int getStartingTimeHour() {
@@ -76,7 +78,7 @@ public class Shift {
     }
     
     /**
-     *
+     * Fetches the starting time minutes of a specific shift
      * @return the starting minutes of the shift as an int
      */
     public int getStartingTimeMinutes() {
@@ -84,7 +86,7 @@ public class Shift {
     }
     
     /**
-     *
+     * Fetches the stopping time of a specific shift
      * @return the stopping time of the shift as a LocalTime
      */
     public LocalTime getStoppingTime() {
@@ -92,7 +94,7 @@ public class Shift {
     }
     
     /**
-     *
+     * Fetches the stopping time hour of a specific shift
      * @return the stopping hour of the shift as an int
      */
     public int getStoppingTimeHour() {
@@ -100,7 +102,7 @@ public class Shift {
     }
     
     /**
-     *
+     * Fetches the stopping time minute of a specific shift
      * @return the stopping minutes of the shift as an int
      */
     public int getStoppingTimeMinutes() {
@@ -108,7 +110,7 @@ public class Shift {
     }
     
     /**
-     *
+     * Fetches the lunch starting time of a specific shift
      * @return the start of lunch as a LocalTime
      */
     public LocalTime getLunchStart() {
@@ -116,7 +118,7 @@ public class Shift {
     }
     
     /**
-     *
+     * Fetches the lunch stopping time of a shift
      * @return the stopping time of lunch as a LocalTime
      */
     public LocalTime getLunchStop() {
@@ -124,7 +126,9 @@ public class Shift {
     }
     
     /**
-     *
+     * Fetches the interval of time you have before and after a specific shift
+     * to determine the time that should be pushed to the beginning or end of a 
+     * shift
      * @return the interval of time you have before and after a shift to
      * determine if the time should be pushed to the beginning or end of a shift
      * as an int
@@ -135,7 +139,8 @@ public class Shift {
     }
     
     /**
-     *
+     * Fetches the time that should be deducted for a lunch break for a specific
+     * shift
      * @return the deduct time for lunch break as an int
      */
     public int getlunchDeduct() {
@@ -144,7 +149,8 @@ public class Shift {
     
 
     /**
-     *
+     * Fetches the grace period for a specific shift which will determine if you
+     * will be too late or early when clocking in or clocking out
      * @return the grace period for checking in late or checking out early as an
      * int
      */
@@ -155,7 +161,8 @@ public class Shift {
     
 
     /**
-     *
+     * Fetches the time after grace period known as the dock which will push you
+     * forwards or backwards in the interval round
      * @return the time after grace period that will push you forward or
      * backwards in the interval round as an int
      */
@@ -168,7 +175,7 @@ public class Shift {
     /* Setter Methods */
 
     /**
-     *
+     * Sets the starting time of a specific shift
      * @param startingTime a LocalTime that represents the start time of a shift
      */
     public void setStartingTime(LocalTime startingTime) {
@@ -176,7 +183,7 @@ public class Shift {
     }
 
     /**
-     *
+     * Sets the stopping time of a specific shift
      * @param stoppingTime a LocalTime that represents the stop time of a shift
      */
     public void setStoppingTime(LocalTime stoppingTime) {
@@ -184,7 +191,7 @@ public class Shift {
     }
 
     /**
-     *
+     * Sets the lunch start time of a specific shift
      * @param lunchStart a LocalTime that represents the start of lunch of a 
      * shift
      */
@@ -193,7 +200,7 @@ public class Shift {
     }
 
     /**
-     *
+     * Sets the lunch stopping time of a specific shift
      * @param lunchStop a LocalTime that represents the end of lunch of a 
      * shift
      */
@@ -202,7 +209,8 @@ public class Shift {
     }
 
     /**
-     *
+     * Sets the time that should be deducted for a lunch break for a specific
+     * shift
      * @param lunchDeduct an int that represents the deducted time of a lunch in
      * a shift
      */
@@ -212,7 +220,9 @@ public class Shift {
 
 
     /**
-     *
+     * Sets the interval of time you have before and after a specific shift
+     * to determine the time that should be pushed to the beginning or end of a 
+     * shift
      * @param interval an int that represents the interval time of a shift
      */
 
@@ -222,7 +232,8 @@ public class Shift {
 
 
     /**
-     *
+     * Sets the grace period for a specific shift which will determine if you
+     * will be too late or early when clocking in or clocking out
      * @param gracePeriod an int that represents the grace period of a shift
      */
 
@@ -232,7 +243,8 @@ public class Shift {
 
 
     /**
-     *
+     * Sets the time after grace period known as the dock which will push you
+     * forwards or backwards in the interval round
      * @param dock an int that represents the amount of dock time of a shift
      */
 
@@ -241,7 +253,7 @@ public class Shift {
     }
     
     /**
-     *
+     * Fetches the total amount of time a specific shift has
      * @return the total amount of time that a shift has as an int
      */
     public int totalShiftHours() {
@@ -253,7 +265,7 @@ public class Shift {
     }
     
     /**
-     *
+     * Fetches the total amount of time you have for lunch in a specific shift
      * @return the total amount of time in lunch as an int
      */
     public int totalLunchTime() {
